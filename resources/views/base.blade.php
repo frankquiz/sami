e4c9c740b19eaf4fee9b6a1c6c7753776b3783c8
oquiroz <otnielquiroz@gmail.com>
2015-08-13 17:11:02 -0430

    Tema
    Instalacion de Tema

---
+++ resources/views/base.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>SAMI</title>
+
+	<!-- Fonts -->
+   <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+   <link rel="shortcut icon" href="favicon.ico" />
+
+   <!-- THEMA ADMIN 
+		========================================= -->
+
+	<!-- Bootstrap Core CSS -->
+    <link href="{{ asset('/tema-admin/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
+
+     <!-- MetisMenu CSS -->
+    <link href="{{ asset('/tema-admin/bower_components/metisMenu/dist/metisMenu.min.css') }}" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="{{ asset('/tema-admin/dist/css/timeline.css') }}" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('/tema-admin/dist/css/sb-admin-2.css') }}" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="{{ asset('/tema-admin/bower_components/morrisjs/morris.css') }}" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="{{ asset('/tema-admin/bower_components/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+
+   
+   
+</head>
+
+<body>
+	<div id="wrapper">
+		@yield('content')
+	</div>
+
+   	<script src="{{ asset('/tema-admin/bower_components/jquery/dist/jquery.min.js') }}"></script>
+
+   	<!-- jQuery -->
+    <script src="{{ asset('/tema-admin/bower_components/jquery/dist/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ asset('/tema-admin/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="{{ asset('/tema-admin/bower_components/metisMenu/dist/metisMenu.min.js') }}"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="{{ asset('/tema-admin/bower_components/raphael/raphael-min.js') }}"></script>
+    <script src="{{ asset('/tema-admin/bower_components/morrisjs/morris.min.js') }}"></script>
+    <script src="{{ asset('/tema-admin/js/morris-data.js') }}"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="{{ asset('/tema-admin/dist/js/sb-admin-2.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/pantallas/contacto.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF8">
+	<title>Pagina de contacto</title>
+</head>
+<body>
+		
+		
+
+
+
+		<!--<h1 align="center">CONTACTAME</h1>
+		<p align="center">Pagina de Enlaces</p>
+
+<div align="center">
+	<div > <a href="{{ url('/pagina') }}">pagina</a></div>
+	<div > <a href="{{ url('/pagina1') }}">pagina1</a></div>
+	<div > <a href="{{ url('/pagina2') }}">pagina2</a></div>
+
+
+	
+	</div>
+	<h2 align="center" style="color:red">Cambio de Prueba</h2>
+-->
+
+</body>
+</html>
